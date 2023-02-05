64dec871e68684038b4079d7f8d01210e50f3404
georgius1024 <georgius@ural.ru>
2023-02-05 22:04:04 +0500

    Practice. Task 1

--- tests/TodoForm.spec.js
+++ tests/TodoForm.spec.js
@@ -5,18 +5,7 @@ import component from "src/components/TodoForm.vue";
 describe("TodoForm component", () => {
   it("mounts without error", () => {
     const wrapper = mount(component);
-    expect(wrapper).toBeDefined();
+    expect(wrapper.exists()).toBeTruthy();
     expect(wrapper.html()).toMatchSnapshot();
   });
-
-  it('handles input and emits events', () => {
-    const wrapper = mount(component);
-    const inputControl = wrapper.find('[data-testid="item-input"]')
-    const buttonControl = wrapper.find('[data-testid="item-submit"]')
-    inputControl.setValue('new item')
-    buttonControl.trigger('click')
-    expect(wrapper.emitted().add).toHaveLength(1)
-    expect(wrapper.emitted().add).toHaveProperty("0.0", "new item")
-  })
-
 });
