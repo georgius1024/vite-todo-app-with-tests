2dab6bca2592e407d49573226f4d6ee09eb9893f
georgius1024 <georgius@ural.ru>
2023-06-08 21:33:16 +0500

    added alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { configDefaults, defineConfig } from "vitest/config";
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [Vue()],
   test: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
     globals: true,
     environment: "jsdom",
       exclude: [...configDefaults.exclude, '**/e2e/**']
