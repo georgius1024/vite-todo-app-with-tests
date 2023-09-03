277665b2b6340050d959e325716a558262c23822
georgius1024 <georgius@ural.ru>
2023-09-03 20:53:30 +0500

    testing with low-level mocking

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import axios from "axios";
 import TodoList from "./components/TodoList.vue";
 import TodoForm from "./components/TodoForm.vue";
 
-const items = ref([
-  "Setup tests",
-  "Create unit tests for services",
-  "Create components tests for components",
-  "Create e2e tests for app",
-  "(*) Add more tests",
-]);
+const items = ref([]);
 
-const addItem = (item) => items.value.push(item)
-const removeItem = (item) => items.value = items.value.filter(e => e !== item)
+const addItem = (item) => items.value.push(item);
+const removeItem = (item) =>
+  (items.value = items.value.filter((e) => e !== item));
+
+const loading = ref(false);
+
+onMounted(async () => {
+  loading.value = true;
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+  items.value = response.data.slice(0, 10);
+  loading.value = false;
+});
 </script>
 
 <template>
   <div class="app">
     <h1>TodoApp</h1>
-    <TodoList :items="items" @remove="removeItem" />
-    <hr />
-    <TodoForm @add="addItem" />
-    <div>Click to add</div>
+    <template v-if="!loading">
+      <TodoList :items="items" @remove="removeItem" />
+      <hr />
+      <TodoForm @add="addItem" />
+      <div>Click to add</div>
+    </template>
+    <template v-else> Loading... </template>
   </div>
 </template>
 
