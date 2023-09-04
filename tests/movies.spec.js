b0e5ce925b188a88d425d28095b54d3c74ed3447
georgius1024 <georgius@ural.ru>
2023-09-04 15:11:34 +0500

    full test

--- tests/movies.spec.js
+++ tests/movies.spec.js
@@ -6,6 +6,9 @@ const baseUrl = "localhost:4000/movies";
 vi.mock("axios", () => ({
   default: {
     get: vi.fn().mockResolvedValue(42),
+    post: vi.fn().mockResolvedValue(42),
+    put: vi.fn().mockResolvedValue(42),
+    delete: vi.fn().mockResolvedValue(42),
   },
 }));
 
@@ -25,5 +28,23 @@ describe("Movies", () => {
     const response = await movies.show(1);
     expect(response).toBe(42);
     expect(axios.get).toBeCalledWith(`${baseUrl}/1`, null);
+  });
+  it("creates movie", async () => {
+    const movies = new Movies(baseUrl);
+    const response = await movies.create({name: 'New movie'});
+    expect(response).toBe(42);
+    expect(axios.post).toBeCalledWith(`${baseUrl}`, {name: 'New movie'});
+  });
+  it("updates movie", async () => {
+    const movies = new Movies(baseUrl);
+    const response = await movies.update(1, {name: 'Updated!'});
+    expect(response).toBe(42);
+    expect(axios.put).toBeCalledWith(`${baseUrl}/1`, {name: 'Updated!'});
+  });
+  it("deletes movie", async () => {
+    const movies = new Movies(baseUrl);
+    const response = await movies.destroy(1);
+    expect(response).toBe(42);
+    expect(axios.delete).toBeCalledWith(`${baseUrl}/1`, null);
   });
 });
